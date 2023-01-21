a3dc45efe2b64b847d649598aad938f9f7c8f297
osawa-koki <github@osawa.dev>
2023-01-21 12:19:49 +0000

    GETとPOST

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,31 @@
-use actix_web::{web, App, HttpServer, Responder};
+use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+use serde::{Deserialize, Serialize};
 
-async fn hello() -> impl Responder {
-  "text:hello"
+#[derive(Deserialize)]
+struct Hello {
+  name: String
+}
+
+#[derive(Serialize)]
+struct Greeting {
+    message: String
+}
+
+async fn hello_get() -> impl Responder {
+  "Hello World 🐙🐙🐙"
+}
+
+async fn hello_post(info: web::Json<Hello>) -> impl Responder {
+  let message = format!("Hello {}", &info.name);
+  HttpResponse::Ok().json(Greeting{message})
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
   HttpServer::new(|| {
     App::new()
-      .route("/api/hello", web::get().to(hello))
+      .route("/api/hello", web::get().to(hello_get))
+      .route("/api/hello", web::post().to(hello_post))
   })
   .bind("0.0.0.0:8080")?
   .run()
