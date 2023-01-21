58cf4fcbfddd50ec548e28897e01f6cbdcef10dd
osawa-koki <github@osawa.dev>
2023-01-21 12:02:20 +0000

    簡単なコード書いた。

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer, Responder};
+
+async fn hello() -> impl Responder {
+  "text:hello"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+  HttpServer::new(|| {
+    App::new()
+      .route("/api/hello", web::get().to(hello))
+  })
+  .bind("0.0.0.0:8080")?
+  .run()
+  .await
 }
